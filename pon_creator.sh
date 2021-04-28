00ae41e2c490d005643d8319989ea46d62339021
vera <60466054+stalvera@users.noreply.github.com>
2021-04-28 14:55:39 +0200

    Jetzt werden statistische Parameter erhoben beim Erstellen des PONs. Die Schritte der Filterung werden in einem separaten Report angezeigt.

--- pon_creator.sh
+++ pon_creator.sh
@@ -5,12 +5,12 @@ if [[ $# -lt 3 ]]; then
 	printf "$0 \n coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n To create a Panel of Normals please enter the following parameters:\n Name of Panel of Normals\n Path to folder with paired-end fastq-files \n Path to gender table\n  Example use: bash pon_creator.sh sample.pon ~/sequence.data/sample.pon/ ~/sequence.data/sample.pon/gender.table.csv\n"
 fi
 
-printf "You started pon_creator.sh - a part of coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n This skript creates Panel of normals tables from paired-end fastq-files. \n"
+printf "You started pon_creator.sh - a part of coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n This skript creates a Panel of normals tables from paired-end fastq-files. \n"
 
 cp -p -v config.txt $2
-cp -p -v create_a_pon.shell.r $2
+cp -p -v report.create.a.pon.and.stats.Rmd $2
 
-printf "PoN data ... "
+printf "PON data ... "
 printf "$1\t" > $2/pon.data.tsv; printf "$2\t" >> $2/pon.data.tsv; printf $3 >> $2/pon.data.tsv
 printf "were recorded\n"
 
@@ -22,15 +22,25 @@ printf "Mapping with Bowtie2 ... \n"
 for f in *_R1_001.fastq.gz; do n=$(echo $f | sed 's/_R1_001.fastq.gz//') ; echo $n; bowtie2 -x $index -p 8 -1 ${n}_R1_001.fastq.gz -2 ${n}_R2_001.fastq.gz | sam2bam $n.bam; done 2> logs.bowtie.txt
 printf "was successful\n"
 
-printf "PoN samples read-table ... "
+printf "PON subjects read-table ... "
 ~/bin/subread-2.0.0-source/bin/featureCounts -a $gtf -o pon.fc.tsv -T $(nproc) *.bam 2> logs.featureCounts.txt
 printf "was created with FeatureCounts\n "
 
-printf "The normalisation and calculation of statistical parameters in R ... "
-R CMD BATCH create_a_pon.shell.r
+printf "GC content of the PON subjects ... "
+samtools faidx $reference -o GRCh38.p13.genome.fa.fai
+cut -f 1,2 GRCh38.p13.genome.fa.fai | sed -n '/chr/p' > GRCh38.p13.genome.sizes
+bedtools makewindows -g GRCh38.p13.genome.sizes -w 1000000 > GRCh38.p13.genome.1M.bed
+bedtools nuc -fi $reference -bed GRCh38.p13.genome.1M.bed > GRCh38.p13.genome.1M.nucl 
+printf "was calculated with bedtools \n"
+
+printf "The normalisation of PON and calculation of statistical parameters in R ... "
+R -e "rmarkdown::render('report.create.a.pon.and.stats.Rmd')"
+#pandoc --data-dir=pwd report.create.a.pon.and.stats.Rmd
 printf "was successful\n"
 
-rm create_a_pon.shell.r
+printf "You can find the report under report.create.a.pon.and.stats.pdf in folder $2 \n "
+
+rm report.create.a.pon.and.stats.Rmd
 rm config.txt
 rm *.bam
 rm *.bai

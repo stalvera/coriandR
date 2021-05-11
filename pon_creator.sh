3051fcd756fc5ad9b16ab0f507370284b5f3a05a
vera <60466054+stalvera@users.noreply.github.com>
2021-05-11 10:28:59 +0200

    report on the pon improved, gc correction introduced, the gonosomes were also normalized to 2

--- pon_creator.sh
+++ pon_creator.sh
@@ -2,17 +2,17 @@
 set -e
 
 if [[ $# -lt 3 ]]; then
-	printf "$0 \n coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n To create a Panel of Normals please enter the following parameters:\n Name of Panel of Normals\n Path to folder with paired-end fastq-files \n Path to gender table\n  Example use: bash pon_creator.sh sample.pon ~/sequence.data/sample.pon/ ~/sequence.data/sample.pon/gender.table.csv\n"
+	printf "$0 \n coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n \n To create a Panel of Normals please enter the following parameters:\n \n Name of Panel of Normals\n Path to folder with paired-end fastq-files \n Path to meta table (gender table)\n \n  Example use: bash pon_creator.sh sample.pon ~/sequence.data/sample.pon/ ~/sequence.data/sample.pon/pon.meta.csv\n \n"
 fi
 
-printf "You started pon_creator.sh - a part of coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n This skript creates a Panel of normals tables from paired-end fastq-files. \n"
+printf "You started pon_creator.sh - a part of coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n This skript creates a Panel of normals tables from paired-end fastq-files and create a report. \n \n"
 
 cp -p -v config.txt $2
 cp -p -v report.create.a.pon.and.stats.Rmd $2
 
 printf "PON data ... "
 printf "$1\t" > $2/pon.data.tsv; printf "$2\t" >> $2/pon.data.tsv; printf $3 >> $2/pon.data.tsv
-printf "were generated\n\n"
+printf "were generated\n \n"
 
 cd $2
 
@@ -20,25 +20,25 @@ source config.txt
 
 printf "Mapping with Bowtie2 ... \n"
 for f in *_R1_001.fastq.gz; do n=$(echo $f | sed 's/_R1_001.fastq.gz//') ; echo $n; bowtie2 -x $index -p 8 -1 ${n}_R1_001.fastq.gz -2 ${n}_R2_001.fastq.gz | sam2bam $n.bam; done 2> logs.bowtie.txt
-printf "was successful\n\n"
+printf "was successful\n \n"
 
 printf "PON subjects read-table ... "
 ~/bin/subread-2.0.0-source/bin/featureCounts -a $gtf -o pon.fc.tsv -T $(nproc) *.bam 2> logs.featureCounts.txt
-printf "was created with FeatureCounts\n\n "
+printf "was created with FeatureCounts\n \n "
 
 printf "GC content of the PON subjects ... "
 samtools faidx $reference -o GRCh38.p13.genome.fa.fai
 cut -f 1,2 $reference.fai | sed -n '/chr/p' > GRCh38.p13.genome.sizes
 bedtools makewindows -g GRCh38.p13.genome.sizes -w 1000000 > GRCh38.p13.genome.1M.bed
 bedtools nuc -fi $reference -bed GRCh38.p13.genome.1M.bed > GRCh38.p13.genome.1M.nucl 
-printf "was calculated with bedtools \n\n"
+printf "was calculated with bedtools \n \n"
 
 printf "The normalisation of PON and calculation of statistical parameters in R ... "
 R -e "rmarkdown::render('report.create.a.pon.and.stats.Rmd')"
 #pandoc --data-dir=pwd report.create.a.pon.and.stats.Rmd
-printf "was successful\n\n"
+printf "was successful\n \n"
 
-printf "You can find the report under report.create.a.pon.and.stats.pdf in folder $2 \n "
+printf "You can find the report under report.create.a.pon.and.stats.pdf in folder $2 \n \n "
 
 rm report.create.a.pon.and.stats.Rmd
 rm config.txt
@@ -49,6 +49,6 @@ rm GRCh38.p13.genome.sizes
 rm GRCh38.p13.genome.1M.bed
 rm GRCh38.p13.genome.1M.nucl
 
-printf "BAM files have been deleted \n\n "
+printf "BAM files have been deleted \n \n "
 
-printf "You can find the PoN table under $1.tsv in folder $2 \n"
+printf "You can find the PoN table under $1.tsv in folder $2 \n \n"
--- pon_creator.wo.mapping.sh
+++ pon_creator.wo.mapping.sh
@@ -2,17 +2,17 @@
 set -e
 
 if [[ $# -lt 3 ]]; then
-	printf "$0 \n coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n To create a Panel of Normals please enter the following parameters:\n Name of Panel of Normals\n Path to folder with paired-end fastq-files \n Path to gender table\n  Example use: bash pon_creator.sh sample.pon ~/sequence.data/sample.pon/ ~/sequence.data/sample.pon/gender.table.csv\n"
+	printf "$0 \n coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n \n To create a Panel of Normals please enter the following parameters:\n \n Name of Panel of Normals\n Path to folder with paired-end fastq-files \n Path to meta table (gender table)\n \n  Example use: bash pon_creator.sh sample.pon ~/sequence.data/sample.pon/ ~/sequence.data/sample.pon/pon.meta.csv\n \n"
 fi
 
-printf "You started pon_creator.sh - a part of coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n This skript creates a Panel of normals tables from paired-end fastq-files. \n"
+printf "You started pon_creator.sh - a part of coriandR: ChrOmosomal abeRration Identifier AND Reporter in R.\n This skript creates a Panel of normals tables from paired-end fastq-files and create a report. \n \n"
 
 cp -p -v config.txt $2
 cp -p -v report.create.a.pon.and.stats.Rmd $2
 
 printf "PON data ... "
 printf "$1\t" > $2/pon.data.tsv; printf "$2\t" >> $2/pon.data.tsv; printf $3 >> $2/pon.data.tsv
-printf "were generated\n"
+printf "were generated\n \n"
 
 cd $2
 
@@ -20,24 +20,24 @@ source config.txt
 
 printf "PON subjects read-table ... "
 ~/bin/subread-2.0.0-source/bin/featureCounts -a $gtf -o pon.fc.tsv -T $(nproc) *.bam 2> logs.featureCounts.txt
-printf "was created with FeatureCounts\n "
+printf "was created with FeatureCounts\n \n"
 
 printf "GC content of the PON subjects ... "
 samtools faidx $reference -o GRCh38.p13.genome.fa.fai
 cut -f 1,2 $reference.fai | sed -n '/chr/p' > GRCh38.p13.genome.sizes
 bedtools makewindows -g GRCh38.p13.genome.sizes -w 1000000 > GRCh38.p13.genome.1M.bed
 bedtools nuc -fi $reference -bed GRCh38.p13.genome.1M.bed > GRCh38.p13.genome.1M.nucl 
-printf "was calculated with bedtools \n"
+printf "was calculated with bedtools \n \n"
 
 printf "The normalisation of PON and calculation of statistical parameters in R ... "
 R -e "rmarkdown::render('report.create.a.pon.and.stats.Rmd')"
 #pandoc --data-dir=pwd report.create.a.pon.and.stats.Rmd
-printf "was successful\n"
+printf "was successful\n \n"
 
-printf "You can find the report under report.create.a.pon.and.stats.pdf in folder $2 \n "
+printf "You can find the report under report.create.a.pon.and.stats.pdf in folder $2 \n \n"
 
 rm report.create.a.pon.and.stats.Rmd
 rm config.txt
 rm pon.data.tsv
 
-printf "You can find the PoN table under $1.tsv in folder $2 \n"
+printf "You can find the PoN table under $1.tsv in folder $2 \n \n"
